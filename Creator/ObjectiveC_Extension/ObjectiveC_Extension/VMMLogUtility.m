d82b7bc7b7f19d17ff60818b3483860608ee3a8b
Dean M Greer <38226388+Gcenx@users.noreply.github.com>
2026-01-20 22:46:53 -0500

    Remove Creator & ObjectiveC_Extension

--- Creator/ObjectiveC_Extension/ObjectiveC_Extension/VMMLogUtility.m
+++
@@ -1,9 +0,0 @@
-//
-//  VMMLogUtility.m
-//  ObjectiveC_Extension
-//
-//  Created by Vitor Marques de Miranda on 19/12/2017.
-//  Copyright © 2017 VitorMM. All rights reserved.
-//
-
-#import "VMMLogUtility.h"
